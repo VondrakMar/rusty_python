84e699ab5cd98ab9f72e743b34de7c6ee5c05359
vondrakmar <vondrakmar@gmail.com>
2024-01-29 00:46:09 +0100

    moving head

---
+++ src/graphcs/models.rs
@@ -0,0 +1,53 @@
+use sdl2::render::Canvas;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::sys::KeyCode;
+use sdl2::video::Window;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+pub struct Python{
+    pub head: Rect,
+    pub v_x: i32,
+    pub v_y: i32,
+    pub head_color: Color
+}
+
+impl Python{    
+    pub fn new(x:i32,y:i32,v_x:i32,v_y:i32)->Self{
+        Self{
+            head: Rect::new(x,y,20,20),
+            v_x: v_x,
+            v_y: v_y,
+            head_color: Color::RGB(5,5,5)
+        }
+    }
+    pub fn render(&self, canvas: &mut Canvas<Window>){
+        canvas.set_draw_color(self.head_color);
+        canvas.fill_rect(self.head).ok().unwrap_or_default();
+    }
+    pub fn change_position(&mut self){
+        // let temp = self.copy();
+        // self.head.set_x(temp.head.x()+temp.v_x);
+        self.head.set_x(self.head.x()+self.v_x);
+        self.head.set_y(self.head.y()+self.v_y);
+    }
+    pub fn change_speed(&mut self, keycode: Option<Keycode>){
+        if keycode.unwrap() == sdl2::keyboard::Keycode::Right {
+            self.v_x += 2;
+        }
+        else if keycode.unwrap() == sdl2::keyboard::Keycode::Left {
+            self.v_x -= 2;
+        }
+        else if keycode.unwrap() == sdl2::keyboard::Keycode::Up {
+            self.v_y -= 2;
+        }
+        else if keycode.unwrap() == sdl2::keyboard::Keycode::Down {
+            self.v_y += 2;
+        }
+    }
+
+    pub fn copy(&self) -> Self{
+        Self{head: self.head, v_x: self.v_x, v_y: self.v_y, head_color: self.head_color}
+    }
+}
+
---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::rect::Rect;
+use std::time::Duration;
+
+mod graphcs;
+use graphcs::models::Python;
+fn main() -> Result<(),String> {
+    let screen_width = 800;
+    let screen_high = 800;
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let window = video_subsystem.window("rusty_python",screen_width,screen_high)
+        .position_centered()
+        .build()
+        .unwrap();
+    let mut canvas = window.into_canvas()
+        .build()
+        .unwrap();    
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    // let mut snake = Rect::new(100,100,10,10);
+    let mut snake = Python::new(20,20,4,4);
+
+    'running: loop {
+        canvas.clear();
+        for event in event_pump.poll_iter(){
+            match event{
+                Event::Quit {..} => {
+                    break 'running;
+                }
+                Event::KeyDown {keycode,..}=>{
+                    snake.change_speed(keycode);
+                }
+                _ => {}
+            }
+        }
+        canvas.set_draw_color(Color::RGB(0, 120, 160));
+        canvas.fill_rect(Rect::new(0,0,screen_width,screen_high)).ok().unwrap_or_default();
+        // snake.set_x(snake.x() + move_horizont);
+        // snake.set_y(snake.y() + move_vertical);
+        // canvas.set_draw_color(Color::RGB(0,0,10));
+        // canvas.fill_rect(snake).ok().unwrap_or_default();
+        snake.change_position();
+        snake.render(&mut canvas);
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+    Ok(())
+}
+
+
